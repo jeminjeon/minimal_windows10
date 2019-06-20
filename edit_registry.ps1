fcb52f88f72256ad1b6d14c2ebbb6a0e3e4e9f81
Jemin Jeon <32831384+jeminjeon@users.noreply.github.com>
2019-06-20 19:17:04 +0000

    Update edit_registry.ps1

--- edit_registry.ps1
+++ edit_registry.ps1
@@ -3,6 +3,9 @@ Computer\HKEY_CLASSES_ROOT\WOW6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1F
 Computer\HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}
 System.IsPinnedToNameSpaceTree = 0
 
+# Delete
+Computer\HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{018D5C66-4533-4307-9B53-224DE2ED1FE6}
+
 # Remove Quick Access from File Explorer Sidebar
 Computer\HKEY_CLASSES_ROOT\CLSID\{679f85cb-0220-4080-b29b-5540cc05aab6}\ShellFolder
 Attributes = a0600000
